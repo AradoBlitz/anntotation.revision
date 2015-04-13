f2f4cd35002a83afa7d84a0bf34b78fb2db1de2d
AradoBlitz <schlachtnavaho@gmail.com>
2015-04-14 01:24:41 +0300

    Implemented Update.extract method which represents case that sometimes class hasn`t the @Revision annotaion and no Update object should be created. No annotated by the @Revision class shoudn`t be represent and processed by the application.

--- test/annotation/revision/GetRevisionTest.java
+++ test/annotation/revision/GetRevisionTest.java
@@ -7,16 +7,28 @@ import org.junit.Test;
 
 public class GetRevisionTest {
 
+	@Revision(name = "Vass",date="30.10.2015",comment="Some comment about update")
+	static class Updated {}
 	
 	@Test
 	public void getDataFromAnnotation() throws Exception {
 		assertNotNull(Updated.class.getAnnotation(Revision.class)); 
 		Update updateFromClass = new Update(Updated.class); 
 		assertEquals("Vass",updateFromClass.name);
-		assertEquals("30.10.2015", updateFromClass.date());
-		assertEquals("Some comment about update", Updated.class.getAnnotation(Revision.class).comment());
+		assertEquals("30.10.2015", updateFromClass.date);
+		assertEquals("Some comment about update",updateFromClass.comment);
+		
+		assertNull("No update object should be returned for NoAnnotated class",Update.extract(NoAnotated.class));
+		assertNotNull("Update object should be returned for Annotated class.",Update.extract(Updated.class));
 		
 	}
 	
+	static class NoAnotated{}
+	
+	@Test
+	public void noAnnotated(){
+		Class noAnnotatedClass = NoAnotated.class;
+		assertNull("getAnnotation should return null.",noAnnotatedClass.getAnnotation(Revision.class));
+	}
 	
 }
