366bbe74b88c6a4e0d27c0a377520e6aefa85971
AradoBlitz <schlachtnavaho@gmail.com>
2015-04-13 10:46:58 +0300

    Implemented test of steps needed to extract revision data from @Revision annotation.

---
+++ test/annotation/revision/GetRevisionTest.java
@@ -0,0 +1,16 @@
+package annotation.revision;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class GetRevisionTest {
+
+	
+	@Test
+	public void start() throws Exception {
+		assertNotNull(Updated.class.getAnnotation(Revision.class)); 
+		assertEquals("Vass",Updated.class.getAnnotation(Revision.class).name());
+	}
+}
