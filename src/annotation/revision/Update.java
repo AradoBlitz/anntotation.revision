dbdb97bac96a69c22a21620253801fd38181a730
AradoBlitz <schlachtnavaho@gmail.com>
2015-04-14 12:52:52 +0300

    Introduced Update class. Made final revision properties. Its mean that Update object becomes immutable object-value.

---
+++ src/annotation/revision/Update.java
@@ -0,0 +1,24 @@
+package annotation.revision;
+
+public class Update {
+
+	public final String name;
+	
+	public final String date;
+	
+	public final String comment;
+
+	private Update(Class updated) {
+		Revision revision = (Revision) updated.getAnnotation(Revision.class);
+		name =  revision.name();
+		date =   revision.date();
+		comment = revision.comment();
+	}
+
+	public static Update extract(Class updated) {
+		if(updated.getAnnotation(Revision.class)!=null)
+			return new Update(updated);
+		return null;
+	}
+
+}
