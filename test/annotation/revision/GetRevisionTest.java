76dc7c24b4c5ee0666d303be9fbd43cc052dc6ba
AradoBlitz <schlachtnavaho@gmail.com>
2015-04-14 13:00:14 +0300

    Extracted tests for getting the Revision Annotation from the Class object. Extracted test for creation Update instance in the application.

--- test/annotation/revision/GetRevisionTest.java
+++ test/annotation/revision/GetRevisionTest.java
@@ -10,20 +10,27 @@ public class GetRevisionTest {
 	@Revision(name = "Vass",date="30.10.2015",comment="Some comment about update")
 	static class Updated {}
 	
+	static class NoAnotated{}
+	
+	@Test
+	public void getRevisionAnnotationFromUpdatedClass(){
+		assertNotNull(Updated.class.getAnnotation(Revision.class));
+	}
+	
 	@Test
 	public void getDataFromAnnotation() throws Exception {
-		assertNotNull(Updated.class.getAnnotation(Revision.class)); 
+		
 		Update updateFromClass = Update.extract(Updated.class); 
 		assertEquals("Vass",updateFromClass.name);
 		assertEquals("30.10.2015", updateFromClass.date);
-		assertEquals("Some comment about update",updateFromClass.comment);
-		
-		assertNull("No update object should be returned for NoAnnotated class",Update.extract(NoAnotated.class));
-		assertNotNull("Update object should be returned for Annotated class.",Update.extract(Updated.class));
-		
+		assertEquals("Some comment about update",updateFromClass.comment);		
 	}
 	
-	static class NoAnotated{}
+	@Test
+	public void updateInstanceCreation(){
+		assertNull("No update object should be returned for NoAnnotated class",Update.extract(NoAnotated.class));
+		assertNotNull("Update object should be returned for Annotated class.",Update.extract(Updated.class));		
+	}
 	
 	@Test
 	public void noAnnotated(){
