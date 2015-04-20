c508a2ea1e06fdc4a8a144ae44cf26da55a1698c
AradoBlitz <schlachtnavaho@gmail.com>
2015-04-20 13:12:23 +0300

    Fixed truncated error by multiple to 13 size buffer.

--- test/annotation/revision/UnpackArchiveTest.java
+++ test/annotation/revision/UnpackArchiveTest.java
@@ -2,10 +2,13 @@ package annotation.revision;
 
 import static org.junit.Assert.*;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
@@ -33,11 +36,38 @@ public class UnpackArchiveTest {
 	
 		public Class toClass(ZipFile zipFile, ZipEntry entry) throws Exception{
 			InputStream entriesInput =  zipFile.getInputStream(entry);
-			byte[] buffer = new byte[1024];
+			byte[] buffer = new byte[1024*13];
 			int read = entriesInput.read(buffer);
 			byte[] b = Arrays.copyOf(buffer, read);
 			return defineClass(entry.getName().replaceAll("/", ".").replaceAll(".class", ""), b , 0, read);
 		}
+
+		public List<Class> extractClasses(String archiveName) throws Exception{
+			ZipFile zipFile = new ZipFile(archiveName);
+			ArrayList<Class> classList = new ArrayList<>();
+			Enumeration<? extends ZipEntry> entries = zipFile.entries();
+			for(;entries.hasMoreElements();){
+				ZipEntry entry = entries.nextElement();
+				if(!entry.getName().contains(".class"))
+					continue;
+				Class class1 = new MyClassLoader().toClass(zipFile,entry);			
+			classList.add(class1);
+			}
+			return classList;
+		}
+
+		public Class toClass(byte[] copyOf) {
+			
+			return defineClass("annotation.revision.UpdateDao", copyOf, 0, copyOf.length);
+		}
+	}
+	
+	@Test
+	public void truncatedClassExample() throws Exception {
+		byte[] buffer = new byte[1024*2];
+		InputStream input = new FileInputStream("/home/dmitriy/Projects/Java/portfolio/annotation.revision/bin/annotation/revision/UpdateDao.class");
+		int read = input.read(buffer);
+		assertNotNull(new MyClassLoader().toClass(Arrays.copyOf(buffer , read)));
 	}
 	
 	@Test
@@ -46,5 +76,11 @@ public class UnpackArchiveTest {
 		Class class1 = new MyClassLoader().toClass(zipFile,zipFile.getEntry(revisionedTestClass));
 		assertNotNull(class1);
 		assertEquals("annotation.revision.UnpackArchiveTest$Revisioned", class1.getName());
+	}
+	
+	@Test
+	public void getClassListFromArchive() throws Exception {
+		
+		assertEquals(9, new MyClassLoader().extractClasses("./etc/Untitled.jar").size());
 	}
 }
