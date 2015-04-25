03de60f9d55c5a94afb4be7d9f1a9fe179bf8157
AradoBlitz <schlachtnavaho@gmail.com>
2015-04-26 00:38:17 +0300

    Implemented add jar functionality.

--- src/annotation/revision/RevisionManager.java
+++ src/annotation/revision/RevisionManager.java
@@ -1,15 +1,22 @@
 package annotation.revision;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 public class RevisionManager {
 
@@ -18,7 +25,7 @@ public class RevisionManager {
 		DefaultTableModel dataModel = new DefaultTableModel(
 				new Object[][]{}
 				,new Object[]{"#","Author","Date","Comment"});
-		
+		JTable table = new JTable(dataModel);
 		try {
 			Class.forName("org.hsqldb.jdbcDriver");
 			Connection connection = DriverManager.getConnection("jdbc:hsqldb:mem:test","sa","");
@@ -36,14 +43,46 @@ public class RevisionManager {
 			e.printStackTrace();
 		}
 		
+		JPanel addJarPanel = new JPanel();
+		JButton addJarSubmit = new JButton("Add Jar");
+		addJarPanel.add(addJarSubmit);
+		addJarSubmit.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser(new File("./"));
+				if(JFileChooser.APPROVE_OPTION == fileChooser.showOpenDialog(addJarPanel))
+					try {
+						new JarArchive()
+							.readPackage(fileChooser.getSelectedFile().getAbsolutePath())
+							.saveTo("jdbc:hsqldb:mem:test","sa","");
+						
+						List<Update> updates = UpdateDao.loadFrom("jdbc:hsqldb:mem:test","sa","");
+						DefaultTableModel dataModel = new DefaultTableModel(
+								new Object[][]{}
+								,new Object[]{"#","Author","Date","Comment"});
+						int i=1;
+						for(Update update: updates)
+							dataModel.addRow(new Object[]{i++,update.name,update.date,update.comment});
+						table.setModel(dataModel);
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				
+			}
+		});
+		
+		mainWindow
+		.getContentPane()
+		.add(addJarPanel,BorderLayout.NORTH);
+	
 		
 		mainWindow
 			.getContentPane()
-			.add(
-					new JScrollPane(
-							new JTable(
-									dataModel))
-					,BorderLayout.NORTH);
+			.add(new JScrollPane(
+					table)
+						,BorderLayout.CENTER);
 		
 		mainWindow.setSize(300, 300);
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
